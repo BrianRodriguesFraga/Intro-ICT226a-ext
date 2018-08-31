186b77d72a3896a0740bdbbced251fae57322413
BrianRodriguesFragaCPNV <Brian.Rodrigues-Fraga@cpnv.ch>
2018-08-31 13:44:08 +0200

    Ajoutez des fichiers projet.

---
+++ tests/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace tests
+{
+    public class Class1
+    {
+        #region variables
+        private static char ope;
+        private static double op1;
+        private static double op2;
+        private static double resultat;
+        #endregion variables
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Entrée le premier nombre : \n");
+            op1 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n\nEntrée l'opérateur : \n");
+            ope = (char)Console.Read();
+
+            Console.Write("\n\nEntrée le second nombre : \n");
+            op2 = int.Parse(Console.ReadLine());
+
+            resultat = op1 + ope + op2;
+
+            Console.Write("\n\nLe résultat est : " + resultat);
+
+            Console.ReadLine();
+        }
+
+        private static int addi(int re1, int re2)
+        {
+            return re1 + re2;
+        }
+
+        private static int sous(int re1, int re2)
+        {
+            return re1 - re2;
+        }
+
+        private static int mult(int re1, int re2)
+        {
+            return re1 * re2;
+        }
+
+        private static double divi(int re1, int re2)
+        {
+
+            return re1 / re2;
+        }
+
+    }
+}
